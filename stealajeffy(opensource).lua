64ec1cd6455f2cda212194141ef5eed9d18052f6
jodta <133034953+jodta@users.noreply.github.com>
2025-09-21 23:11:11 -0400

    Update stealajeffy(opensource).lua

--- stealajeffy(opensource).lua
+++ stealajeffy(opensource).lua
@@ -1,9 +1,15 @@
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-local Admin = player:WaitForChild("PlayerGui"):WaitForChild("Admin")
-for _, child in ipairs(Admin:GetDescendants()) do
-    if child:IsA("GuiObject") then
-        child.Visible = true
+if _G.adminmenu == nil then _G.adminmenu = false end
+
+spawn(function()
+    local player = game:GetService("Players").LocalPlayer
+    local Admin = player:WaitForChild("PlayerGui"):WaitForChild("Admin")
+    while true do
+        for _, child in ipairs(Admin:GetDescendants()) do
+            if child:IsA("GuiObject") then
+                child.Visible = _G.adminmenu
+            end
+        end
+        Admin.Visible = _G.adminmenu
+        task.wait(0.2)
     end
-end
-Admin.Visible = true
+end)
